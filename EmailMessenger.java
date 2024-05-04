7641ac4c467788c49daf621fbb0be54aa2df442c
svchocki <145773118+svchocki@users.noreply.github.com>
2024-05-04 11:51:24 +0200

    Create EmailMessenger.java

---
+++ EmailMessenger.java
@@ -0,0 +1,11 @@
+package company.implementations;
+
+import company.interfaces.Messenger;
+
+public class EmailMessenger implements Messenger {
+
+    @Override
+    public void sendMessage(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
