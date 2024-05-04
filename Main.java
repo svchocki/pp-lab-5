e03cade238e954d0f3178fd8fbb866ec2a8edbfe
svchocki <145773118+svchocki@users.noreply.github.com>
2024-05-04 11:55:10 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,14 +1,8 @@
-import company.beans.Person;
-import company.exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            Person person = new Person("John", 30);
-            System.out.println("Name: " + person.getName());
-            System.out.println("Age: " + person.getAge());
-        } catch (InvalidAgeException e) {
-            System.out.println("Error: " + e.getMessage());
-        }
+        EmailMessenger emailMessenger = new EmailMessenger();
+        emailMessenger.sendMessage("Hello World");
     }
 }
