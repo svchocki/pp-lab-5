d463fa11136602fbb60450f07b5d431af916cf9d
svchocki <145773118+svchocki@users.noreply.github.com>
2024-05-04 11:56:32 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,15 @@
 import company.implementations.EmailMessenger;
+import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+        
+        int sum = MathUtils.add(num1, num2);
+        System.out.println("Sum of " + num1 + " and " + num2 + " is: " + sum);
+        
         EmailMessenger emailMessenger = new EmailMessenger();
-        emailMessenger.sendMessage("Hello World");
+        emailMessenger.sendMessage("The sum of " + num1 + " and " + num2 + " is: " + sum);
     }
 }
